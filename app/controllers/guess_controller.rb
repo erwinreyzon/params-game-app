e0047bb795c27abb7d7d1edaf828d57cc2cae5ec
erwin <winzon353535@gmail.com>
2022-03-10 11:13:15 -0800

    needs fix

--- app/controllers/guess_controller.rb
+++ app/controllers/guess_controller.rb
@@ -1,12 +1,56 @@
 class GuessController < ApplicationController
 
-  def a_method
-
+  def cap_method
+  
     name = params[:input]
-    if name[1] == "a"
-      render json: "Hey, your name starts with the first letter of the alphabet!"
-    end
+    output_msg = "Your name is #{name.upcase}."
+    render json: {message: output_msg}
 
   end
+
+  #wrong, rewrite
+  # def a_method
+
+  #   name = params[:input]
+  #   output_msg = "Your name is #{name}."
+  #   render json: {message: output_msg}
+  #   if name.upcase.starts_with?("A")
+  #     render json: "Hey, your name starts with the first letter of the alphabet!"
+  #   end
+
+  # end
+
+  #wrong, rewrite
+  # def guess_query
+
+  #   winning_number = 32
+  #   input_guess = params[:users_guess].to_i
+  #   if input_guess = winning_number
+  #     output_message = "you win"
+  #   end
+  #   render json: {message: output_message}
+    
+  # end
+
+  def add_method
+    number = params[:number].to_i
+    number2 = params[:second_number].to_i
+    sum = number + number2
+    render json: "#{number} + #{number2} = #{sum}"
+  end
+
+  #wrong, rewrite
+  # def guess_method
+  #   winning_number = 32
+  #   number = params[:number].to_i
+  #   if number > winning_number
+  #     message = "Too high"
+  #   elsif number < winning_number
+  #     message = "too low"
+  #   else
+  #     message = "you win"
+  #   end
+
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get "/caps" => "guess#cap_method"
   get "/a" => "guess#a_method"
+  get "/guess_query" => "guess#guess_query"
+
+  get "/sum/:number/:second_number" => "guess#add_method"
+  get "/guess/:number" => "guess#guess_method"
 
 end
